8b1f9d6ce97a974e0a642c2b0f78fddcd9f80e91
Jason Savelli <jsavelli@splio.com>
2024-09-14 20:43:42 +0200

    fix: large navbar animation

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -6,12 +6,11 @@ type AppLayoutProps = {
 
 export default function AppLayout({ children }: AppLayoutProps) {
   return (
-    <div className="flex w-full flex-1">
+    <div className="flex w-full flex-1 flex-col xl:flex-row">
       <Navigation />
 
       {/* Adding some padding bottom on smaller screen to compensate the fixed navigation bar */}
-      {/* absolute position otherwise width is badly computed due to nav being fixed */}
-      <div className="absolute flex min-h-[100dvh] w-full flex-1 flex-col px-4 py-6 pb-[76px] transition-transform md:px-10 md:py-8 md:pb-[102px] xl:w-[calc(100%-88px)] xl:translate-x-[88px] xl:pb-8 xl:peer-data-[expanded=true]:w-[calc(100%-300px)] xl:peer-data-[expanded=true]:translate-x-[300px]">
+      <div className="flex min-h-[100dvh] w-full flex-1 flex-col px-4 py-6 pb-[76px] md:px-10 md:py-8 md:pb-[102px] xl:pb-8">
         {children}
       </div>
     </div>
--- app/app/navigation/navigation.tsx
+++ app/app/navigation/navigation.tsx
@@ -48,91 +48,99 @@ export const Navigation = () => {
   }, [])
 
   return (
-    <section
-      className={cn(
-        "peer fixed bottom-0 z-50 flex w-full flex-col rounded-t-lg bg-primary px-4 pt-2 transition-all md:px-10 xl:left-0 xl:top-0 xl:w-[300px] xl:rounded-l-none xl:rounded-r-2xl xl:pb-6 xl:pl-0 xl:pr-6 xl:pt-10",
-        {
-          "xl:w-[88px] xl:pr-0": !isExpanded,
-        },
-      )}
-      data-expanded={DEFAULT_IS_EXPANDED}
-      ref={navigationRef}
-    >
-      <div
-        className={cn("hidden justify-center px-8 xl:flex", {
-          "justify-start": isExpanded,
+    <>
+      <section
+        className={cn("navigation-bar", {
+          "navigation-bar-collapsed": !isExpanded,
         })}
+        data-expanded={DEFAULT_IS_EXPANDED}
+        ref={navigationRef}
       >
-        <LogoComponent className="mb-16 text-white" />
-      </div>
-
-      <nav>
-        <ul className="flex justify-around xl:flex-col xl:justify-start">
-          <NavigationLink
-            icon={<Home className="h-6 w-6" />}
-            isExpanded={isExpanded}
-            isActive={pathname === "/app"}
-            href="/app"
-          >
-            Overview
-          </NavigationLink>
-
-          <NavigationLink
-            icon={<ArrowsDownUp className="h-6 w-6" />}
-            isExpanded={isExpanded}
-            isActive={pathname.startsWith("/app/transactions")}
-            href="/app/transactions"
-          >
-            Transactions
-          </NavigationLink>
-
-          <NavigationLink
-            icon={<ChartDonut className="h-6 w-6" />}
-            isExpanded={isExpanded}
-            isActive={pathname.startsWith("/app/budgets")}
-            href="/app/budgets"
-          >
-            Budgets
-          </NavigationLink>
-
-          <NavigationLink
-            icon={<JarFill className="h-6 w-6" />}
-            isExpanded={isExpanded}
-            isActive={pathname.startsWith("/app/pots")}
-            href="/app/pots"
-          >
-            Pots
-          </NavigationLink>
-
-          <NavigationLink
-            icon={<Receipt className="h-6 w-6" />}
-            isExpanded={isExpanded}
-            isActive={pathname.startsWith("/app/recurring-bills")}
-            href="/app/recurring-bills"
-          >
-            Recurring bills
-          </NavigationLink>
-        </ul>
-      </nav>
-
-      <button
-        className="mt-auto hidden xl:block"
-        type="button"
-        onClick={handleToggleSidebar}
-      >
-        <NavigationItem
-          icon={
-            <ArrowFatLinesLeft
-              className={cn("h-6 w-6 transition-transform", {
-                "rotate-180": !isExpanded,
-              })}
-            />
-          }
-          isExpanded={isExpanded}
+        <div
+          className={cn("hidden justify-center px-8 xl:flex", {
+            "justify-start": isExpanded,
+          })}
         >
-          Minimize Menu
-        </NavigationItem>
-      </button>
-    </section>
+          <LogoComponent className="mb-16 text-white" />
+        </div>
+
+        <nav>
+          <ul className="flex justify-around xl:flex-col xl:justify-start">
+            <NavigationLink
+              icon={<Home className="h-6 w-6" />}
+              isExpanded={isExpanded}
+              isActive={pathname === "/app"}
+              href="/app"
+            >
+              Overview
+            </NavigationLink>
+
+            <NavigationLink
+              icon={<ArrowsDownUp className="h-6 w-6" />}
+              isExpanded={isExpanded}
+              isActive={pathname.startsWith("/app/transactions")}
+              href="/app/transactions"
+            >
+              Transactions
+            </NavigationLink>
+
+            <NavigationLink
+              icon={<ChartDonut className="h-6 w-6" />}
+              isExpanded={isExpanded}
+              isActive={pathname.startsWith("/app/budgets")}
+              href="/app/budgets"
+            >
+              Budgets
+            </NavigationLink>
+
+            <NavigationLink
+              icon={<JarFill className="h-6 w-6" />}
+              isExpanded={isExpanded}
+              isActive={pathname.startsWith("/app/pots")}
+              href="/app/pots"
+            >
+              Pots
+            </NavigationLink>
+
+            <NavigationLink
+              icon={<Receipt className="h-6 w-6" />}
+              isExpanded={isExpanded}
+              isActive={pathname.startsWith("/app/recurring-bills")}
+              href="/app/recurring-bills"
+            >
+              Recurring bills
+            </NavigationLink>
+          </ul>
+        </nav>
+
+        <button
+          className="mt-auto hidden xl:block"
+          type="button"
+          onClick={handleToggleSidebar}
+        >
+          <NavigationItem
+            icon={
+              <ArrowFatLinesLeft
+                className={cn("h-6 w-6 transition-transform", {
+                  "rotate-180": !isExpanded,
+                })}
+              />
+            }
+            isExpanded={isExpanded}
+          >
+            Minimize Menu
+          </NavigationItem>
+        </button>
+      </section>
+
+      {/* Empty element with same dimensions as the desktop navbar
+          Required to "reset" the fixed position and keep a smooth animation
+      */}
+      <div
+        className={cn("navigation-bar relative z-0 hidden xl:flex", {
+          "navigation-bar-collapsed": !isExpanded,
+        })}
+      />
+    </>
   )
 }

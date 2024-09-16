828649e3c48de71a47f4c451e4a752ddfa41983b
Jason Savelli <jsavelli@splio.com>
2024-09-17 00:34:14 +0200

    task: run tsc and eslint when commiting

--- app/app/transactions/transactions-types.ts
+++ app/app/transactions/transactions-types.ts
@@ -5,4 +5,4 @@ export type Transaction = Database["public"]["Tables"]["transactions"]["Row"]
 
 export type TransactionListItem = NonNullable<
   Awaited<ReturnType<typeof fetchTransactions>>["transactions"]
->
+>[number]

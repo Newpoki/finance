32a3e4c656cb7b471a1a5bd360a74889cb286fa6
Jason Savelli <jsavelli@splio.com>
2024-09-22 11:47:30 +0200

    feat: improve transaction overview error screen

--- app/app/overview/@dialog/transactions/[transactionId]/edit/error.tsx
+++ app/app/overview/@dialog/transactions/[transactionId]/edit/error.tsx
@@ -2,12 +2,22 @@
 
 import { DialogTitle } from "@/components/ui/dialog"
 import { OverviewTransactionsDialogWrapper } from "./overview-transactions-dialog-wrapper"
+import ErrorIllustration from "@/public/illustrations/error-illustration.svg"
 
 export default function OverviewTransactionsEditDialogError() {
-  // TODO: Create real error page
   return (
     <OverviewTransactionsDialogWrapper redirectUrl="/app/overview">
-      <DialogTitle>Oh no, anyway</DialogTitle>
+      <DialogTitle>Edit Transaction</DialogTitle>
+      <div className="flex flex-1 flex-col">
+        <ErrorIllustration className="max-w-[400px] xl:max-w-[600px]" />
+        <div className="flex w-full flex-col items-center text-center">
+          <h2 className="font-bold">Oh no, something is broken</h2>
+          <p className="mb-4">
+            An unexpected error happen, thus we don&apos;t know why it id. Close
+            the dialog and try again later.
+          </p>
+        </div>
+      </div>
     </OverviewTransactionsDialogWrapper>
   )
 }

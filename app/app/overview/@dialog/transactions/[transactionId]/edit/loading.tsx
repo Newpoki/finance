f453f3b84e8289f058ed5b7e62874a2cda57ac3d
Jason Savelli <jsavelli@splio.com>
2024-09-22 11:43:01 +0200

    feat: improve transaction overview loading screen

--- app/app/overview/@dialog/transactions/[transactionId]/edit/loading.tsx
+++ app/app/overview/@dialog/transactions/[transactionId]/edit/loading.tsx
@@ -1,13 +1,31 @@
 "use client"
 
-import { DialogTitle } from "@/components/ui/dialog"
+import {
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog"
 import { OverviewTransactionsDialogWrapper } from "./overview-transactions-dialog-wrapper"
+import { Skeleton } from "@/components/ui/skeleton"
 
 export default function OverviewTransactionsEditDialogLoading() {
-  // TODO: Create real loading page
   return (
     <OverviewTransactionsDialogWrapper redirectUrl="/app/overview">
-      <DialogTitle>Loading</DialogTitle>
+      <DialogHeader>
+        <DialogTitle>Edit Transaction</DialogTitle>
+        <DialogDescription>
+          Choose a category to set a transaction. These categories can help you
+          monitor spending.
+        </DialogDescription>
+      </DialogHeader>
+
+      <div className="flex flex-col gap-4">
+        <Skeleton className="h-11 w-[280px]" />
+        <Skeleton className="h-11 w-full" />
+        <Skeleton className="h-11 w-full" />
+        <Skeleton className="h-11 w-full" />
+        <Skeleton className="h-11 w-full" />
+      </div>
     </OverviewTransactionsDialogWrapper>
   )
 }

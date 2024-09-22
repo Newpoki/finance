da09c1acdfb0623108d17d1c971c6b88f486113f
Jason Savelli <jsavelli@splio.com>
2024-09-22 11:08:19 +0200

    task: remove suspense when reloading transactions list filters

--- app/app/transactions/loading.tsx
+++ app/app/transactions/loading.tsx
@@ -1,5 +1,6 @@
 import { Paper } from "@/components/ui/paper"
-import { TransactionsSuspenseFallback } from "./transactions-suspense-fallback"
+import { Skeleton } from "@/components/ui/skeleton"
+import range from "lodash.range"
 
 /**
  * This is the displayed component the first time the `/transactions` page is fetched
@@ -10,7 +11,24 @@ export default function TransactionsLoading() {
       <h1>Transactions</h1>
 
       <Paper className="flex flex-1 flex-col gap-6">
-        <TransactionsSuspenseFallback />
+        <div className="mb-4 flex items-center justify-between gap-6">
+          <Skeleton className="h-11 w-full max-w-[320px]" />
+
+          <div className="flex items-center gap-6">
+            <Skeleton className="h-6 w-6 md:h-11 md:w-[170px]" />
+            <Skeleton className="h-6 w-6 md:h-11 md:w-[230px]" />
+          </div>
+        </div>
+
+        <ul className="flex flex-1 flex-col gap-2">
+          {range(10).map((item) => {
+            return (
+              <li key={item}>
+                <Skeleton className="h-14" />
+              </li>
+            )
+          })}
+        </ul>
       </Paper>
     </div>
   )
--- app/app/transactions/page.tsx
+++ app/app/transactions/page.tsx
@@ -2,8 +2,6 @@ import { Paper } from "@/components/ui/paper"
 import { TransactionsFilters } from "./filters/transactions-filters"
 import { TransactionsList } from "./list/transactions-list"
 import { parseTransactionsSearchParams } from "./parse-transactions-search-params"
-import { Suspense } from "react"
-import { TransactionsSuspenseFallback } from "./transactions-suspense-fallback"
 
 type TransactionPageProps = {
   searchParams: unknown
@@ -14,24 +12,14 @@ export default function TransactionsPage({
 }: TransactionPageProps) {
   const parsedSearchParams = parseTransactionsSearchParams(searchParams)
 
-  // As weird as it is, it's the recommended way to
-  // display a fallback when updating search parameters
-  // https://nextjs.org/learn/dashboard-app/adding-search-and-pagination
-  const stringifiedSearchParams = JSON.stringify(parsedSearchParams)
-
   return (
     <div className="flex w-full flex-1 flex-col gap-8">
       <h1>Transactions</h1>
 
       <Paper className="flex flex-1 flex-col gap-6">
-        <Suspense
-          fallback={<TransactionsSuspenseFallback />}
-          key={stringifiedSearchParams}
-        >
-          <TransactionsFilters parsedSearchParams={parsedSearchParams} />
+        <TransactionsFilters parsedSearchParams={parsedSearchParams} />
 
-          <TransactionsList parsedSearchParams={parsedSearchParams} />
-        </Suspense>
+        <TransactionsList parsedSearchParams={parsedSearchParams} />
       </Paper>
     </div>
   )

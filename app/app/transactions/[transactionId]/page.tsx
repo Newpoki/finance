1be8343178d406fb83de7f1a79c9918660b58a10
Jason Savelli <jsavelli@splio.com>
2024-09-23 08:52:53 +0200

    feat: add go back button on new transaction and edit transaction pages

--- app/app/transactions/[transactionId]/page.tsx
+++ app/app/transactions/[transactionId]/page.tsx
@@ -2,6 +2,9 @@ import { Paper } from "@/components/ui/paper"
 import { fetchCurrentUserProfile } from "../../profile/fetch-current-user-profile"
 import { TransactionForm } from "../../transaction/transaction-form"
 import { fetchTransactionById } from "../../transaction/fetch-transaction-by-id"
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+import CarretLeft from "@/icons/carret-left.svg"
 
 type TransactionPageProps = {
   params: {
@@ -23,7 +26,20 @@ export default async function TransactionPage({
   //   TODO: Add erorr page for edit transaction
   return (
     <div className="flex w-full flex-1 flex-col gap-8">
-      <h1>Edit Transaction</h1>
+      <div className="flex items-center justify-between gap-4">
+        <h1>Edit Transaction</h1>
+
+        <Link href="/app/transactions">
+          <Button className="flex md:hidden" size="icon" variant="ghost">
+            <CarretLeft />
+          </Button>
+          <Button className="hidden gap-2 md:flex">
+            <CarretLeft />
+
+            <span>Go Back</span>
+          </Button>
+        </Link>
+      </div>
 
       <Paper className="flex flex-1 flex-col gap-6">
         <TransactionForm
--- app/app/transactions/new/page.tsx
+++ app/app/transactions/new/page.tsx
@@ -1,13 +1,29 @@
 import { Paper } from "@/components/ui/paper"
 import { TransactionForm } from "../../transaction/transaction-form"
 import { fetchCurrentUserProfile } from "../../profile/fetch-current-user-profile"
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+import CarretLeft from "@/icons/carret-left.svg"
 
 export default async function TransactionsAddNewPage() {
   const currentUserProfile = await fetchCurrentUserProfile()
 
   return (
     <div className="flex w-full flex-1 flex-col gap-8">
-      <h1>Add Transaction</h1>
+      <div className="flex items-center justify-between gap-4">
+        <h1>Add Transaction</h1>
+
+        <Link href="/app/transactions">
+          <Button className="flex md:hidden" size="icon" variant="ghost">
+            <CarretLeft />
+          </Button>
+          <Button className="hidden gap-2 md:flex">
+            <CarretLeft />
+
+            <span>Go Back</span>
+          </Button>
+        </Link>
+      </div>
 
       {/* TODO: Add error page */}
       <Paper className="flex flex-1 flex-col gap-6">

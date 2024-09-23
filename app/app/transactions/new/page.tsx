4814fa7ccf6cfdc370e8ad666e49020f7a66fc75
Jason Savelli <jsavelli@splio.com>
2024-09-23 08:59:53 +0200

    task: Go back buttons now using router.back to keep parameters

--- app/app/transactions/new/page.tsx
+++ app/app/transactions/new/page.tsx
@@ -1,9 +1,7 @@
 import { Paper } from "@/components/ui/paper"
 import { TransactionForm } from "../../transaction/transaction-form"
 import { fetchCurrentUserProfile } from "../../profile/fetch-current-user-profile"
-import Link from "next/link"
-import { Button } from "@/components/ui/button"
-import CarretLeft from "@/icons/carret-left.svg"
+import { TransactionNewGoBackButton } from "./transaction-new-go-back-button"
 
 export default async function TransactionsAddNewPage() {
   const currentUserProfile = await fetchCurrentUserProfile()
@@ -13,16 +11,7 @@ export default async function TransactionsAddNewPage() {
       <div className="flex items-center justify-between gap-4">
         <h1>Add New Transaction</h1>
 
-        <Link href="/app/transactions">
-          <Button className="flex md:hidden" size="icon" variant="ghost">
-            <CarretLeft />
-          </Button>
-          <Button className="hidden gap-2 md:flex">
-            <CarretLeft />
-
-            <span>Go Back</span>
-          </Button>
-        </Link>
+        <TransactionNewGoBackButton />
       </div>
 
       <Paper className="flex flex-1 flex-col gap-6">
---
+++ app/app/transactions/new/transaction-new-go-back-button.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { useRouter } from "next/navigation"
+import { useCallback } from "react"
+import CarretLeft from "@/icons/carret-left.svg"
+
+export const TransactionNewGoBackButton = () => {
+  const router = useRouter()
+
+  const handleClick = useCallback(() => {
+    router.back()
+  }, [router])
+
+  return (
+    <>
+      <Button
+        className="flex md:hidden"
+        size="icon"
+        variant="ghost"
+        type="button"
+        onClick={handleClick}
+      >
+        <CarretLeft />
+      </Button>
+
+      <Button
+        className="hidden gap-2 md:flex"
+        type="button"
+        onClick={handleClick}
+      >
+        <CarretLeft />
+
+        <span>Go Back</span>
+      </Button>
+    </>
+  )
+}
